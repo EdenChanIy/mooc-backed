5f1afff465da53b96319af13973b2e73421aa2e5
chenjh <1301793698@qq.com>
2019-03-03 18:22:39 +0800

    适配打包

--- src/main/java/com/sysu/mooc_backed/MoocBackedApplication.java
+++ src/main/java/com/sysu/mooc_backed/MoocBackedApplication.java
@@ -3,10 +3,18 @@ package com.sysu.mooc_backed;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.sysu.mooc_backed.dao")
-public class MoocBackedApplication {
+public class MoocBackedApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MoocBackedApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(MoocBackedApplication.class, args);
