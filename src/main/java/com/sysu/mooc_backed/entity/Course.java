896ed7a4ffbf317ac646a4bf1fea24bfb06ccd5f
EdenChanIy <chenjh246@mail2.sysu.edu.cn>
2019-03-16 23:04:32 +0800

    课程实体类

---
+++ src/main/java/com/sysu/mooc_backed/entity/Course.java
@@ -0,0 +1,7 @@
+package com.sysu.mooc_backed.entity;
+
+/**
+ * 课程
+ */
+public class Course {
+}
---
+++ src/main/java/com/sysu/mooc_backed/entity/CourseLabel.java
@@ -0,0 +1,7 @@
+package com.sysu.mooc_backed.entity;
+
+/**
+ * 课程分类
+ */
+public class CourseLabel {
+}
