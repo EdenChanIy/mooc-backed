036ac2b679399357a49d6c982767a99efb583847
EdenChanIy <chenjh246@mail2.sysu.edu.cn>
2019-04-09 22:15:30 +0800

    fix(filter): 增加过滤器解决跨域访问问题

--- src/main/java/com/sysu/mooc_backed/controller/UserController.java
+++ src/main/java/com/sysu/mooc_backed/controller/UserController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 public class UserController {
 
