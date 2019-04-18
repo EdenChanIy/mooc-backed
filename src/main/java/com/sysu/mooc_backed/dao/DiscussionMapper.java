bbaa22a199a6fc6afda2105d38f0c548be69db71
EdenChanIy <chenjh246@mail2.sysu.edu.cn>
2019-04-18 22:05:05 +0800

    2019.4.18

---
+++ src/main/java/com/sysu/mooc_backed/dao/DiscussionMapper.java
@@ -0,0 +1,54 @@
+package com.sysu.mooc_backed.dao;
+
+import com.sysu.mooc_backed.entity.Discussion;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Author: EdenChanIy
+ * @Date: 2019/4/18 16:58
+ */
+
+@Repository
+public interface DiscussionMapper {
+
+    //根据id获取讨论信息
+    @Select("SELECT * FROM discussion WHERE id = #{id}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "replyCount", column = "reply_count"),
+            @Result(property = "likeCount", column = "like_count"),
+            @Result(property = "view", column = "view"),
+            @Result(property = "authorId", column = "author_id"),
+            @Result(property = "pid", column = "pid"),
+            @Result(property = "time", column = "time"),
+            @Result(property = "createTime", column = "create_time")
+    })
+    Discussion findDiscussionById(int id);
+
+    //根据课时id获取该课时下的讨论列表
+    @Select("SELECT * FROM discussion WHERE pid = #{pid}")
+    @Results({
+            @Result(property = "id", column = "id"),
+            @Result(property = "title", column = "title"),
+            @Result(property = "replyCount", column = "reply_count"),
+            @Result(property = "likeCount", column = "like_count"),
+            @Result(property = "view", column = "view"),
+            @Result(property = "authorId", column = "author_id"),
+            @Result(property = "pid", column = "pid"),
+            @Result(property = "time", column = "time"),
+            @Result(property = "createTime", column = "create_time")
+    })
+    List<Discussion> findListByPId(int pid);
+
+    //根据课时id获取该课时下的讨论数
+    @Select("SELECT COUNT(*) FROM discussion WHERE pid = #{pid}")
+    int findListCountByPid(int pid);
+
+    
+}
---
+++ src/main/java/com/sysu/mooc_backed/entity/Discussion.java
@@ -0,0 +1,93 @@
+package com.sysu.mooc_backed.entity;
+
+import java.sql.Date;
+import java.sql.Time;
+
+/**
+ * @Author: EdenChanIy
+ * @Date: 2019/4/18 16:52
+ * 讨论
+ */
+public class Discussion {
+    private int id;
+    private String title;
+    private int replyCount; //讨论回复数
+    private int likeCount; //讨论获赞数
+    private int view; //浏览数
+    private int authorId; //讨论发起者id
+    private int pid; //该讨论对应课时id
+    private Time time; //讨论对应时间节点
+    private Date createTime; //讨论创建时间
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getReplyCount() {
+        return replyCount;
+    }
+
+    public void setReplyCount(int replyCount) {
+        this.replyCount = replyCount;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public int getView() {
+        return view;
+    }
+
+    public void setView(int view) {
+        this.view = view;
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
